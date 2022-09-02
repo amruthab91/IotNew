e892d2f47147073c169dfb36ccf1e772eab25e7d
amruthab91 <92167987+amruthab91@users.noreply.github.com>
2022-09-02 19:14:02 +0530

    Add files via upload

---
+++ ecg.py
@@ -0,0 +1,55 @@
+import random
+import time
+
+# Using the Python Device SDK for IoT Hub:
+#   https://github.com/Azure/azure-iot-sdk-python
+# The sample connects to a device-specific MQTT endpoint on your IoT Hub.
+from azure.iot.device import IoTHubDeviceClient, Message
+
+# The device connection string to authenticate the device with your IoT hub.
+# Using the Azure CLI:
+# az iot hub device-identity show-connection-string --hub-name {YourIoTHubName} --device-id MyNodeDevice --output table
+CONNECTION_STRING = "HostName=IotProjectEcg.azure-devices.net;DeviceId=IOT;SharedAccessKey=dIPmzJZPtbko32OxxYHCfo/Naqyz699lU/ljQejwpug="
+
+# Define the JSON message to send to IoT Hub.
+PRESSURE = 20.0
+PRESSURE1 = 60
+MSG_TXT = '{{"pressure": {pressure},"pressure1": {pressure}}}'
+
+def iothub_client_init():
+    # Create an IoT Hub client
+    client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
+    return client
+
+def iothub_client_telemetry_sample_run():
+
+    try:
+        client = iothub_client_init()
+        print ( "IoT Hub device sending periodic messages, press Ctrl-C to exit" )
+        while True:
+            # Build the message with simulated telemetry values.
+            pressure = PRESSURE + (random.random() * 15)
+            pressure1 = PRESSURE1 + (random.random() * 20)
+            msg_txt_formatted = MSG_TXT.format(pressure=pressure, pressure1=pressure1)
+            message = Message(msg_txt_formatted)
+
+            # Add a custom application property to the message.
+            # An IoT hub can filter on these properties without access to the message body.
+            if pressure > 40:
+              message.custom_properties["pressureAlert"] = "true"
+            else:
+              message.custom_properties["pressureAlert"] = "false"
+
+            # Send the message.
+            print( "Sending message: {}".format(message) )
+            client.send_message(message)
+            print ( "Message successfully sent" )
+            time.sleep(3)
+
+    except KeyboardInterrupt:
+        print ( "IoTHubClient sample stopped" )
+
+if __name__ == '__main__':
+    print ( "IoT Hub Quickstart #1 - Simulated device" )
+    print ( "Press Ctrl-C to exit" )
+    iothub_client_telemetry_sample_run()
