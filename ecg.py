8a76f218be0b6f04aaf1b972763cead4b7e4718e
amruthab91 <92167987+amruthab91@users.noreply.github.com>
2022-09-02 19:47:15 +0530

    Update ecg.py

--- ecg.py
+++ ecg.py
@@ -12,11 +12,9 @@ from azure.iot.device import IoTHubDeviceClient, Message
 CONNECTION_STRING = "HostName=IotProjectEcg.azure-devices.net;DeviceId=IOT;SharedAccessKey=dIPmzJZPtbko32OxxYHCfo/Naqyz699lU/ljQejwpug="
 
 # Define the JSON message to send to IoT Hub.
-BP = 5.0
-PULSE = 0.5
-PRESSURE = 2
-PRESSURE1 = 6.5
-MSG_TXT = '{{"BP": {BP},"PULSE": {PULSE},"PRESSURE":{PRESSURE},"PRESSURE1": {PRESSURE1}}}'
+PRESSURE = 20.0
+PRESSURE1 = 60
+MSG_TXT = '{{"pressure": {pressure},"pressure1": {pressure1}}}'
 
 def iothub_client_init():
     # Create an IoT Hub client
@@ -30,19 +28,17 @@ def iothub_client_telemetry_sample_run():
         print ( "IoT Hub device sending periodic messages, press Ctrl-C to exit" )
         while True:
             # Build the message with simulated telemetry values.
-            bp = BP + (random.random() * 15)
-            pulse = PULSE + (random.random() * 15)
-            pressure= PRESSURE + (random.random() * 15)
-            pressure1 =PRESSURE1 + (random.random() * 15)
-            msg_txt_formatted = MSG_TXT.format(bp=bp, pulse=pulse,pressure=pressure , pressure1=pressure1)
+            pressure = PRESSURE + (random.random() * 15)
+            pressure1 = PRESSURE1 + (random.random() * 20)
+            msg_txt_formatted = MSG_TXT.format(pressure=pressure, pressure1=pressure1)
             message = Message(msg_txt_formatted)
 
             # Add a custom application property to the message.
             # An IoT hub can filter on these properties without access to the message body.
-            #if temperature > 30:
-              #message.custom_properties["temperatureAlert"] = "true"
+            #if pressure > 40:
+              #message.custom_properties["pressureAlert"] = "true"
             #else:
-              #message.custom_properties["temperatureAlert"] = "false"
+              #message.custom_properties["pressureAlert"] = "false"
 
             # Send the message.
             print( "Sending message: {}".format(message) )
